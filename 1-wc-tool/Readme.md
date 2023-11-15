540a3284cc849a7b504476dac9f002afabfa64d0
satyam.sundaram <satyam.sundaram@thoughtspot.com>
2023-11-16 01:02:54 +0530

    [1-wc-tool] add support for stdin

---
+++ 1-wc-tool/Readme.md
@@ -0,0 +1,3 @@
+## Usage
+
+```go build -o myWc . && ./myWc <flag> <filename>```
--- 1-wc-tool/main.go
+++ 1-wc-tool/main.go
@@ -10,10 +10,14 @@ import (
 
 // Flags
 var (
-	inputFlag = flag.String("", "stdin", "Input type")
-	byteFlag  = flag.String("c", "no_init", "count the number of bytes in a file")
-	lineFlag  = flag.String("l", "no_init", "count the number of lines in a file")
-	wordFlag  = flag.String("w", "no_init", "count the number of words in a file")
+	//inputFlag = flag.String("", "stdin", "Input type")
+	//byteFlag  = flag.String("c", "no_init", "count the number of bytes in a file")
+	//lineFlag  = flag.String("l", "no_init", "count the number of lines in a file")
+	//wordFlag  = flag.String("w", "no_init", "count the number of words in a file")
+	inputFlag = flag.Bool("", false, "Input type")
+	byteFlag  = flag.Bool("c", false, "count the number of bytes in a file")
+	lineFlag  = flag.Bool("l", false, "count the number of lines in a file")
+	wordFlag  = flag.Bool("w", false, "count the number of words in a file")
 )
 
 // PanicOnError panics if the error is not nil.
@@ -23,8 +27,8 @@ func PanicOnError(err error) {
 	}
 }
 
-// IsValidFlag checks if the file exists.
-func IsValidFlag(fileName string) bool {
+// FileExists checks if the file exists.
+func FileExists(fileName string) bool {
 	if fileName == "no_init" {
 		return false
 	}
@@ -40,13 +44,7 @@ func IsValidFlag(fileName string) bool {
 // It makes use of a `processor` function to process the buffer and count
 // the occurrence of the required entity. The steps were common for
 // counting bytes, lines, and words.
-func CountEntity(fileName string, processor func([]byte) int) int {
-	// open the file for reading
-	file, err := os.Open(fileName)
-	PanicOnError(err)
-	defer func() {
-		PanicOnError(file.Close())
-	}()
+func CountEntity(file *os.File, processor func([]byte) int) int {
 	// read the file in as a stream of bytes and process the read data
 	counter := 0
 	for {
@@ -66,15 +64,15 @@ func CountEntity(fileName string, processor func([]byte) int) int {
 }
 
 // countBytes counts the number of bytes in a file.
-func countBytes(fileName string) int {
-	return CountEntity(fileName, func(buffer []byte) int {
+func countBytes(file *os.File) int {
+	return CountEntity(file, func(buffer []byte) int {
 		return len(buffer)
 	})
 }
 
 // countLines counts the number of lines in a file.
-func countLines(fileName string) int {
-	return CountEntity(fileName, func(buffer []byte) int {
+func countLines(file *os.File) int {
+	return CountEntity(file, func(buffer []byte) int {
 		count := 0
 		for i := 0; i < len(buffer); i++ {
 			if rune(buffer[i]) == '\n' {
@@ -86,9 +84,9 @@ func countLines(fileName string) int {
 }
 
 // countWords counts the number of words in a file.
-func countWords(fileName string) int {
+func countWords(file *os.File) int {
 	processingWord := false
-	return CountEntity(fileName, func(buffer []byte) int {
+	return CountEntity(file, func(buffer []byte) int {
 		count := 0
 		for i := 0; i < len(buffer); i++ {
 			if unicode.IsSpace(rune(buffer[i])) {
@@ -104,6 +102,25 @@ func countWords(fileName string) int {
 	})
 }
 
+// GetTargetFile opens the required file for processing.
+func GetTargetFile(fileName string) (file *os.File, err error) {
+	// check if the input is from a file & the file exists.
+	// higher priority is given to the file (than stdin)
+	if len(fileName) > 0 {
+		if !FileExists(fileName) {
+			os.Exit(1)
+		}
+		return os.Open(fileName)
+	}
+
+	// check if the input is from stdin
+	fi, _ := os.Stdin.Stat()
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		return os.Stdin, nil
+	}
+	return nil, fmt.Errorf("no input file specified")
+}
+
 func main() {
 	/*
 		Flags
@@ -112,16 +129,22 @@ func main() {
 			-w : count the number of words in a file
 	*/
 	flag.Parse()
-	answer := -1
 
-	if IsValidFlag(*byteFlag) {
-		answer = countBytes(*byteFlag)
-	} else if IsValidFlag(*lineFlag) {
-		answer = countLines(*lineFlag)
-	} else if IsValidFlag(*wordFlag) {
-		answer = countWords(*wordFlag)
+	file, err := GetTargetFile(flag.Arg(0))
+	PanicOnError(err)
+	defer func() {
+		PanicOnError(file.Close())
+	}()
+
+	answer := -1
+	if *byteFlag {
+		answer = countBytes(file)
+	} else if *lineFlag {
+		answer = countLines(file)
+	} else if *wordFlag {
+		answer = countWords(file)
 	} else {
-		fmt.Println("Invalid flag")
+		fmt.Println("No flag specified")
 		os.Exit(1)
 	}
 	fmt.Println(answer)
